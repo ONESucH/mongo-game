bad4155146a349e65618f18f29f5f50beb4f89ea
Bogdanov Ilya <bogdanov@comepay.ru>
2018-04-13 11:29:20 +0300

    Добавил изменения поворота косела при нажатии

--- src/app/common/components/game-table/game-table.component.ts
+++ src/app/common/components/game-table/game-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 
 @Component({
@@ -9,6 +9,7 @@ import {Http} from '@angular/http';
 export class GameTableComponent implements OnInit {
 
   public userArrData: object;
+  public counter = 0;
 
   constructor(private http: Http) {}
 
@@ -20,6 +21,11 @@ export class GameTableComponent implements OnInit {
 
         console.log('this.userArrData', this.userArrData);
       });
+  }
+
+  randomNumber() {
+    this.counter += Math.floor(Math.random() * (1080 - 360 + 1) + 1); // max/min(360deg);
+    return this.counter; // Количество оборотов в карусели
   }
 
 }
