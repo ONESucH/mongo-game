cc1657abf279c7b7cb532ff159a54fb4707afe1e
Bogdanov Ilya <bogdanov@comepay.ru>
2018-04-13 17:16:53 +0300

    Ведутся работы над выйснением позиции

--- src/app/common/components/game-table/game-table.component.ts
+++ src/app/common/components/game-table/game-table.component.ts
@@ -9,7 +9,9 @@ import {Http} from '@angular/http';
 export class GameTableComponent implements OnInit {
 
   public userArrData: object;
-  public counter = 0;
+  public counter: number = 0;
+  private winCounter: number = 0;
+  private root = false;
 
   constructor(private http: Http) {}
 
@@ -23,9 +25,32 @@ export class GameTableComponent implements OnInit {
       });
   }
 
-  randomNumber() {
-    this.counter += Math.floor(Math.random() * (1080 - 360 + 1) + 1); // max/min(360deg);
-    return this.counter; // Количество оборотов в карусели
+  /* Рандомное число */
+  randomNumber($event) {
+    if (!this.root) {
+      $event.target.classList.add('button-active');
+      let bol = this;
+      bol.root = true;
+      setTimeout(function () {
+        bol.root = false;
+        bol.getNumberOfChars();
+        $event.target.classList.remove('button-active');
+      }, 10000);
+      this.counter += Math.floor(Math.random() * (360 - 0) + 1); // max/min(720deg);
+      console.log('this.counter', this.counter);
+      this.winCounter = Math.floor(this.counter / (360 / 8 + 5) / 2);
+      console.log('this.winCounter', this.winCounter);
+      return this.counter; // Количество оборотов в карусели
+    }
+  }
+
+  /* Запишем результат в окно результата */
+  getNumberOfChars() {
+    let pointer = document.querySelector('.pointer');
+
+    console.log('pointer.nodeValue', pointer.nodeValue);
+    console.log('pointer.nodeType', pointer.nodeType);
+    console.log('pointer.nodeType', pointer.tagName);
   }
 
 }
