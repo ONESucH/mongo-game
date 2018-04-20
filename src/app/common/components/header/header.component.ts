126c742e751ff3c6ae9b1940e2b196a1ca20e925
Bogdanov Ilya <bogdanov@comepay.ru>
2018-04-20 15:16:40 +0300

    Обновляю данные при их динамическом изменении в отдельном Views

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -2,8 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 
 import AppNews from '../../../templateComponents/app-news/app-news.js';
-
-const appNewsArr = AppNews.arrNews();
+import {GameTableComponent} from '../game-table/game-table.component';
 
 @Component({
   selector: 'app-header',
@@ -12,17 +11,28 @@ const appNewsArr = AppNews.arrNews();
 })
 export class HeaderComponent implements OnInit {
 
+  private appNewsArr = AppNews.arrNews();
   public userArrData: object;
-  public getNew = appNewsArr[Math.floor(Math.random() * (appNewsArr.length + 1 - 0))];
+  public getNew = this.appNewsArr[Math.floor(Math.random() * (this.appNewsArr.length + 1 - 0))];
   private timer = new Date();
 
   constructor(private http: Http) {}
 
   ngOnInit() {
+    /* Делаем запрос при загрузке документа */
     this.http.get('/registration')
       .subscribe((req) => {
         let json = req.json();
         this.userArrData = json[0];
       });
+
+    /* Запрос по событию */
+    GameTableComponent.updateButton.subscribe(() => {
+      this.http.get('/registration')
+        .subscribe((req) => {
+          let json = req.json();
+          this.userArrData = json[0];
+        });
+    });
   }
 }
